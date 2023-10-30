7999e1b25192cd79802286564ebc613d9ec1e359
Luis Fernando <luisfernando20140@outlook.com>
2023-10-29 22:45:03 -0300

    Initial

---
+++ web-spring-api/src/main/java/com/web/spring/api/WebSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.web.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringApiApplication.class, args);
+	}
+
+}
---
+++ web-spring-api/src/test/java/com/web/spring/api/WebSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.spring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
