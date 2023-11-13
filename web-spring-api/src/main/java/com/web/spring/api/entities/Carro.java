a9d35ce4a0de5c0be5d16bea0118941c631d8239
renan01234 <150536815+renan01234@users.noreply.github.com>
2023-11-13 03:02:36 -0300

    Create Carro.java

---
+++ web-spring-api/src/main/java/com/web/spring/api/entities/Carro.java
@@ -0,0 +1,69 @@
+package com.web.spring.api.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="carros")
+public class Carro {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String modelo;
+
+    @Column(nullable = false)
+    private String marca;
+
+    @Column(nullable = false)
+    private int ano;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    public Carro(){}
+
+    public Carro(String modelo, String marca, int ano, String categoria) {
+        super();
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
